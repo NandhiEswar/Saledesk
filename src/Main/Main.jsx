3fa9684e68611cbb03c45b315e66d0eee97afb9a
Shanmukh <105598377+NandhiEswar@users.noreply.github.com>
2025-04-11 12:35:06 +0530

    add this main page some code

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import "../Navbar/Navbar.css"
 
 function Main() {
-  return (
-    <div>Main</div>
+    return (
+      <section>
+      <div className='h-full w-full bg-red-800'>
+          <div className='flex flex-col items-center justify-center text-center py-20 px-10 '>
+              <h1 className='text-6xl text-white font-sans leading-relaxed'>Supercharge Your Sales – Smarter, Faster, Stronger Sales <span className='text-blue-500 '>SaleDesk Software</span></h1>
+              <p className='text-white py-10 text-xl font-sans'>Maximize sales efficiency with automated workflows, advanced sales insights, integrated communication, and powerful automation.</p>
+
+              <button className='p-3 px-10 rounded-full text-black bg-white'>Request demo</button>
+          </div>
+          
+    </div>
+        <div className='flex fle-col text-gray-400 justify-between px-10 py-20'>
+        <h1>This is the text on the left side </h1>
+        <p>this is the text on  the right side </p>
+            </div>
+            </section>
   )
 }
-
+ 
 export default Main
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import "./Navbar.css"
 
 function Navbar() {
   return (
       <div>
-          <div>
-              
+          <div className='h-20 w-full bg-white flex flex-row justify-between items-center px-20 shadow-2xl'>
+              <img src="https://sconexsoft.com/images/logo-1.png"  />
+              <button className='pill-button'>Request demo</button>
+             
           </div>
+          <hr className='h-1 bg-red-700'/>
     </div>
   )
 }
